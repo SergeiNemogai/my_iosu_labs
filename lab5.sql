9c12aa7e110a64199cde3b77345b84679c97df0c
Sergei <51096050+SergeiNemogai@users.noreply.github.com>
2019-12-15 22:46:27 +0300

    Added DDL and system trigger

--- lab5.sql
+++ lab5.sql
@@ -1,3 +1,5 @@
+-- ALTER TRIGGER имя_триггера DISABLE;
+
 /*
 Написать DML-триггер, регистрирующий изменение данных (вставку,
 обновление, удаление) в одной из таблиц БД. Во вспомогательную таблицу
@@ -42,15 +44,12 @@ BEGIN
 END;
 /
 
-CREATE OR REPLACE TRIGGER TRIGGER_DML
+CREATE OR REPLACE TRIGGER DML_TRIGGER
 AFTER INSERT OR UPDATE OR DELETE
 ON Suppliers
 FOR EACH ROW
-
 DECLARE
-
 op CHAR(1) := 'I';
-
 BEGIN
     CASE
         WHEN INSERTING
@@ -78,10 +77,69 @@ BEGIN
 
     END CASE;
 
-END TRIGGER_DML;
+END DML_TRIGGER;
 /
 
 -- Проверка результатов
 INSERT INTO Suppliers (title, city, tnumber) VALUES ('TEST_TITLE', 'TEST_CITY', 'TEST_TNUMBER');
 UPDATE Suppliers SET city='TEST_CITY_2' WHERE mcode=101;
 DELETE FROM Suppliers WHERE mcode=101;
+
+/*
+Написать DDL-триггер, протоколирующий действия пользователей по
+созданию, изменению и удалению таблиц в схеме во вспомогательную таблицу
+LOG2 в определенное время и запрещающий эти действия в другое время
+*/
+
+CREATE TABLE LOG2(
+    table_name VARCHAR2(30),
+    operation_name VARCHAR2(20),
+    username VARCHAR2(20),
+    operation_date DATE);
+
+
+CREATE OR REPLACE TRIGGER DDL_TRIGGER
+BEFORE CREATE OR ALTER OR DROP ON SCHEMA
+BEGIN
+IF TO_NUMBER(TO_CHAR(SYSDATE,'HH24')) >= 8 AND TO_NUMBER(TO_CHAR(SYSDATE,'HH24')) <= 18 THEN
+    INSERT INTO LOG2(table_name, operation_name, username, operation_date)
+    VALUES (ORA_DICT_OBJ_NAME, ORA_SYSEVENT, USER, SYSDATE);
+ELSE
+    RAISE_APPLICATION_ERROR(
+        num => -20000,
+        msg => 'DDL operations are not provided at this time. Try it later');
+END IF;
+END;
+/
+
+-- Проверки работы триггера
+
+CREATE TABLE Example(example_id INTEGER, example_title VARCHAR2(20));
+DROP TABLE Example;
+ALTER TABLE Example ADD author VARCHAR2(30);
+
+/*
+Написать системный триггер, добавляющий запись во вспомогательную
+таблицу LOG3, когда пользователь подключается или отключается. В таблицу
+логов записывается имя пользователя (USER), тип активности (LOGON или
+LOGOFF), дата (SYSDATE), количество записей в основной таблице БД.
+*/
+
+CREATE TABLE LOG3(
+    username VARCHAR2(20),
+    action_type VARCHAR(10),
+    action_date DATE);
+    
+CREATE OR REPLACE TRIGGER LOGON_TRIGGER AFTER LOGON ON SCHEMA
+BEGIN
+    INSERT INTO LOG3(username, action_type, action_date)
+    VALUES (USER, 'LOGON', SYSDATE);
+END;
+/
+  
+CREATE OR REPLACE TRIGGER LOGOFF_TRIGER BEFORE LOGOFF ON SCHEMA
+BEGIN
+    INSERT INTO LOG3(username, action_type, action_date)
+    VALUES (USER, 'LOGOFF', SYSDATE);
+END;
+/
